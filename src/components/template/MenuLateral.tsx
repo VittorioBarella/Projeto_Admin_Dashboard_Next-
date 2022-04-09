59a03592396dab8588c7085734086d3918e55195
VittorioBarella <vittorio.br@hotmail.com>
2022-04-08 21:13:18 -0300

    Configured the color scheme

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,34 +1,34 @@
 
 import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from '../../icons';
+import Logo from "./Logo";
 import MenuItem from './MenuItem';
 export default function MenuLateral() {
-    return(
-        <aside className="flex flex-col">
+
+    return (
+        <aside className={`
+            flex flex-col
+            bg-gray-200 text-gray-700
+            dark:bg-gray-900
+        `}>
             <div className={`
-                flex
-                flex-col
-                items-center
-                justify-center
-                bg-gradient-to-r
-                from-indigo-500
-                to-purple-800
-                h-20
-                w-20
+                flex flex-col items-center justify-center
+                bg-gradient-to-r from-indigo-500 to-purple-800
+                h-20 w-20
             `}>
+                <Logo />
             </div>
-            <ul className='flex-grow'>
-                <MenuItem url="/" texto="Início" icone={IconeCasa}/>
-                <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes}/>
-                <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino}/>
+            <ul className="flex-grow">
+                <MenuItem url="/" texto="Início" icone={IconeCasa} />
+                <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+                <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
             </ul>
             <ul>
-                <MenuItem 
-                    texto='Sair' 
-                    icone={IconeSair} 
-                    onClick={() => console.log('logout')}
+                <MenuItem
+                    texto="Sair" icone={IconeSair} 
                     className={`
-                        text-red-600
+                        text-red-600 dark:text-red-400
                         hover:bg-red-400 hover:text-white
+                        dark:hover:text-white
                     `}
                 />
             </ul>
--- src/components/template/Titulo.tsx
+++ src/components/template/Titulo.tsx
@@ -8,15 +8,13 @@ export default function Titulo(props:TituloProps) {
     return (
         <div>
             <h1 className={`
-                font-black 
-                text-3xl
-                text-gray-900
+                font-black text-3xl
+                text-gray-900 dark:text-gray-100
 
             `}>{props.titulo}</h1>
             <h2 className={`
-                font-light
-                text-sm
-                text-gray-600
+                font-light text-sm
+                dark:text-gray-300
             `}>{props.subtitulo}</h2>
         </div>
     )
