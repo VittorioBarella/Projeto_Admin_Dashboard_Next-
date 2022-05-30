efed4dbf0784e2eeecf399aa4234a08315b4f922
VittorioBarella <vittorio.br@hotmail.com>
2022-05-29 22:24:53 -0300

    fixed the dark mode text colors

--- src/components/cards/card-stats.jsx
+++ src/components/cards/card-stats.jsx
@@ -12,18 +12,18 @@ export default function CardStats({
 }) {
   return (
     <>
-      <div className='relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg'>
-        <div className='flex-auto p-4'>
-          <div className='flex flex-wrap'>
-            <div className='relative w-full pr-4 max-w-full flex-grow flex-1'>
-              <h5 className='text-blueGray-400 uppercase font-bold text-xs'>
+      <div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+        <div className="flex-auto p-4">
+          <div className="flex flex-wrap">
+            <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
+              <h5 className="text-blueGray-400 uppercase font-bold text-xs dark:text-black">
                 {statSubtitle}
               </h5>
-              <span className='font-semibold text-xl text-blueGray-700'>
+              <span className="font-semibold text-xl text-blueGray-700 dark:text-black">
                 {statTitle}
               </span>
             </div>
-            <div className='relative w-auto pl-4 flex-initial'>
+            <div className="relative w-auto pl-4 flex-initial">
               <div
                 className={
                   "text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full " +
@@ -34,20 +34,20 @@ export default function CardStats({
               </div>
             </div>
           </div>
-          <p className='text-sm text-blueGray-400 mt-4'>
+          <p className="text-sm text-blueGray-400 mt-4 dark:text-black">
             <span className={statPercentColor + " mr-2"}>
               <i
                 className={
                   statArrow === "up"
                     ? "fas fa-arrow-up"
                     : statArrow === "down"
-                    ? "fas fa-arrow-down"
+                    ? "fas fa-arrow-down "
                     : ""
                 }
               ></i>{" "}
               {statPercent}%
             </span>
-            <span className='whitespace-nowrap'>{statDescripiron}</span>
+            <span className="whitespace-nowrap">{statDescripiron}</span>
           </p>
         </div>
       </div>
