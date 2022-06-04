6bf7ba958f1b2aa8cd92542eb67dfdf36a3d4093
VittorioBarella <vittorio.br@hotmail.com>
2022-06-04 20:35:14 -0300

    fixed the background color for the settings component

--- src/pages/health.tsx
+++ src/pages/health.tsx
@@ -3,7 +3,16 @@ import Layout from "../components/template/layout";
 export default function Health() {
   return (
     <Layout title="Health" subTitle="Manage your Health information!">
-      <h1>Health test</h1>
+      <div
+        className={`
+          flex flex-col w-full
+          bg-white text-gray-800 rounded-md
+        `}
+      >
+        <div className="p-6">
+          <h1>Teste</h1>
+        </div>
+      </div>
     </Layout>
   );
 }
